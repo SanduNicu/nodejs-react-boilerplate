a1527e0187da4afe015fdb6ec8bab684612378b8
3x3cu7or <sandu.nicu94@gmail.com>
2019-01-27 19:47:50 +0200

    added react plate and linter

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index-bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translates CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html'
+    })
+  ],
+  resolve: {
+    modules: [path.resolve('./client'), path.resolve('./node_modules')]
+  }
+};
