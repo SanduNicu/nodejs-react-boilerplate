6bf75a51293c8045be60c8758d926b20190d5caa
SanduNicu <sandu.nicu94@gmail.com>
2019-02-02 16:32:54 +0200

    added body parser

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('dist'));
+
 app.get('/api/test', (req, res) => res.send({ test: 'This is a test' }));
+
 app.listen(8080, () => console.log('Listening on port 8080!'));
