0b969d54965cba82d457cfda6ac77e9237d0817a
SanduNicu <sandu.nicu94@gmail.com>
2019-03-28 22:01:02 +0200

    update style file structure

--- client/index.js
+++ client/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import 'styles/index.scss';
+import 'styles/style.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
