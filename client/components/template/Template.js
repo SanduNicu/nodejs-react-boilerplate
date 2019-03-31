87c3e7bacbc2119d676a2a650868fa30d381c16f
SanduNicu <sandu.nicu94@gmail.com>
2019-03-31 10:52:34 +0300

    added template components

---
+++ client/components/template/Template.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Template extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Template
+      </div>
+    );
+  }
+}
+
+export default Template;
--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Application from './components/Application';
 
 import 'styles/style.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Application />, document.getElementById('root'));
