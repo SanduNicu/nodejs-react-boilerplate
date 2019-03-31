462e74c6d76798fb5c9ca2e149c3df33705bdaf9
SanduNicu <sandu.nicu94@gmail.com>
2019-03-31 11:50:57 +0300

    added redux

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Application from './components/Application';
+import Application from 'components/Application';
+import { Provider } from 'react-redux';
+import store from 'store';
 
 import 'styles/style.scss';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Application />
+  </Provider>,
+  document.getElementById('root')
+);
